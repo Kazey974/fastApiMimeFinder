7421165b8222e07f8bb781d46b319763678d0274
Denis LAM-YAM <denis.lamyam@live.fr>
2024-03-01 16:17:56 +0400

    MimeFinder

---
+++ app/analyze.py
@@ -0,0 +1,12 @@
+from starlette.datastructures import UploadFile
+from tempfile import NamedTemporaryFile
+import magic
+
+async def analyze_content(file: UploadFile):
+    content = await file.read()
+    file.close()
+
+    temp = NamedTemporaryFile(delete=False)
+    temp.write(content)
+    temp.close()
+    return magic.from_file(temp.name, mime=True)
---
+++ app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Request
+from starlette.datastructures import UploadFile
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
+from analyze import analyze_content
+import uvicorn
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static", html=True, follow_symlink=True), name="static")
+
+@app.get("/", include_in_schema=False)
+def index():
+    return FileResponse("app/static/index.html")
+
+@app.post("/mime-type", tags=["Files"], status_code=200)
+async def get_mime_type(request: Request):
+    """
+    Get mimetype of files passed through forms or curl
+    """
+    response = dict()
+
+    async with request.form() as form:
+        for file in form.multi_items():
+            if isinstance(file[1], UploadFile):
+                response[file[1].filename] = await analyze_content(file[1])
+
+    return response
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port="8080")
